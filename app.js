35499ba8243afe3b6d3b04a1ce3a0642e7edda01
Diana Garcia-Martinez <dianagarcia1316@gmail.com>
2024-02-22 20:28:43 -0500

    firstAttempt

---
+++ app.js
@@ -0,0 +1,7 @@
+// Keyword --- name -- ()=> {}
+const changeButton =()=> {
+    // this is where I put my instructions
+    let container =
+document.querySelection('.lowerContainer')
+    container.innerHTML="HEY!! This works"
+}
